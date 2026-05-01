2ac6d2e378a4f73fd5c2fa28547692d3e42bdd44
omega-beep <ayushi.supergal89@gmail.com>
2026-05-02 03:57:16 +0530

    remove admin restriction

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { createProject, getProjects } = require("../controllers/projectController");
-const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+  createProject,
+  getProjects,
+} = require("../controllers/projectController");
 
-// ONLY ADMIN CAN CREATE PROJECT
-router.post("/", protect, adminOnly, createProject);
+const { protect } = require("../middleware/authMiddleware");
 
-// BOTH CAN VIEW
+
+router.post("/", protect, createProject);
 router.get("/", protect, getProjects);
 
 module.exports = router;
