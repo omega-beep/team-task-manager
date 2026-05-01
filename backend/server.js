ae3afe94098b673c7ca2591a07651cca513e5afb
omega-beep <ayushi.supergal89@gmail.com>
2026-05-02 04:23:08 +0530

    fix cors properly

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,8 @@ require("dotenv").config();
 const app = express();
 
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"], // your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json());
 
@@ -25,18 +20,15 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
-// test route
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
-// DB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-// server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
